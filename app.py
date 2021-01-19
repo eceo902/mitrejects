ed5875d85be84757e22a04b77b068201d9e96572
eceo902 <evan.seeyave@gmail.com>
2021-01-19 00:23:47 -0500

    Code

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return redirect("https://www.caltech.edu")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
