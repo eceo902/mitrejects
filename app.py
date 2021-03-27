4a3083c981371418b2869ef253d99568d552c42c
eceo902 <evan.seeyave@gmail.com>
2021-03-27 14:21:23 -0400

    Randomization feature

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-website_list = ["https://www.harvard.edu", "https://www.caltech.edu", "https://www.gatech.edu"]
+website_list = ["https://www.harvard.edu", "https://www.caltech.edu", "https://www.gatech.edu", "https://www.stanford.edu"]
 
 prime = (2**11) - 1
 
 @app.route("/")
 def home():
     curr_date = datetime.today()
-    year = curr_date.year
+    year = curr_date.year + 10000
     month = curr_date.month
     day = curr_date.day
     rand = ((year*month*day) % prime) % len(website_list)
     website = website_list[rand]
     return redirect(website)
-    
+
 
 if __name__ == "__main__":
     app.run(debug=False)
