effcb56bb798b5c3e94550959a2dca441f1f63b8
eceo902 <evan.seeyave@gmail.com>
2021-03-27 14:00:42 -0400

    adding to gitignore

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, redirect
+from datetime import timedelta, datetime
+import random
 
 app = Flask(__name__)
 
+
+website_list = ["https://www.harvard.edu", "https://www.caltech.edu", "https://www.gatech.edu"]
+
+
 @app.route("/")
 def home():
-    return redirect("https://www.caltech.edu")
+    
+    return redirect(website)
 
 
 if __name__ == "__main__":
